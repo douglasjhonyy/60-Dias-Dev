d3a6d093ca3da65aef28cbd4b317cf658d45506e
Jhony <douglas.jhonysousa@gmail.com>
2025-12-13 16:23:12 -0300

    add SWITCH

--- Semana 2/dia-09-condicionais/script.js
+++ Semana 2/dia-09-condicionais/script.js
@@ -92,3 +92,55 @@ const conceito = pontos >= 90 ? "A" : pontos >= 80 ? "B" : pontos >= 70 ? "C" : 
 console.log("Conceito:", conceito);
 
 // PARTE 2: SWITCH/CASE
+
+console.log("Switch/Case");
+
+const diaSemana = 3;
+
+switch (diaSemana){
+    case 1:
+        console.log("Segunda-Feira");
+        break;
+    case 2:
+        console.log("Terça-Feira");
+        break;
+    case 3:
+        console.log("Quarta-Feira");
+        break;
+    case 4: 
+        console.log("Quinta-Feira");
+        break;
+    case 5:
+        console.log("Sexta-feira");
+        break;
+    case 6: 
+        console.log("Sábado");
+        break;
+    case 7:
+        console.log("Domingo");
+        break;
+    default:
+        console.log("Dia inválido");
+}
+
+// SWITCH com strings
+
+const comando = "start";
+
+switch (comando){
+    case "start":
+        console.log("Iniciando...");
+        break;
+    case "stop":
+        console.log("Parando...");
+        break;
+    case "pause":
+        console.log("Pausando...");
+        break;
+    default:
+        console.log("Comando desconhecido");
+}
+
+// QUANDO USAR IF vs SWITCH?
+// IF: Comparações complexas, ranges, operadores lógicos
+// SWITCH: Múltiplas opções específicas, mais legível
